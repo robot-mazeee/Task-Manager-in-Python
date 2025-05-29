3f8538775449ed017fd8dce1e961ddf66f5e8e11
robot-mazeee <madalenacmo@gmail.com>
2025-05-29 23:18:40 +0100

    valid_task_number and naming convention corrections

--- TaskManagerTutorial.py
+++ TaskManagerTutorial.py
@@ -10,7 +10,7 @@ tasks = []
 descriptions = {}
 
 # Auxiliary functions
-def displayTasks(all_tasks, descriptions):
+def display_tasks(all_tasks, descriptions):
     print('\nYour tasks: ')
 
     if len(all_tasks) <= 0:
@@ -19,75 +19,71 @@ def displayTasks(all_tasks, descriptions):
         for index, task in enumerate(all_tasks):
             print(f'{index+1}: {task} - {descriptions[task]}')
 
-def newOperation(all_tasks, descriptions):
-    print(descriptions)
+def new_operation(all_tasks, descriptions):
     operation = input("\nPress 'A' to add a new task, 'E' to edit a task, 'R' to remove a task or 'F' to quit the application: ")
 
     if operation == 'A':
-        addTask(all_tasks, descriptions)
+        add_task(all_tasks, descriptions)
     elif operation == 'E':
-        editTask(all_tasks, descriptions)
+        edit_task(all_tasks, descriptions)
     elif operation == 'R':
-        removeTask(all_tasks, descriptions)
+        remove_task(all_tasks, descriptions)
     elif operation == 'F':
         return
     else:
-        newOperation(all_tasks, descriptions)
+        new_operation(all_tasks, descriptions)
 
-def validTaskNumber(all_tasks, operation):
-    task_number = input(f'Enter the number of the task you want to {operation}: ')
-
-    valid = False
-    while not valid:
+def valid_task_number(all_tasks, operation):
+    while True:
+        task_number = input(f'Enter the number of the task you want to {operation}: ')
         try:
-            number = int(task_number)
-            valid = True
+            task_number = int(task_number)
+            if (0 < task_number <= len(all_tasks)):
+                break
+            else:
+                print('Task not found! Please enter a valid task number.\n')
         except:
-            task_number = input('Please provide a valid task number: ')
+            print('Invalid input! Please enter a valid task number.\n')
 
-    if not (0 < number <= len(all_tasks)):
-        print('Task not found!')
-        validTaskNumber(all_tasks, operation)
-    else:
-        return number
+    return task_number
 
 # Operations
-def addDescription(task, descriptions):
+def add_description(task, descriptions):
     description = input('Add a description: ')
     descriptions[task] = description
 
     return descriptions
 
-def editTask(all_tasks, descriptions):
-    task_number = validTaskNumber(all_tasks, 'edit')
+def edit_task(all_tasks, descriptions):
+    task_number = valid_task_number(all_tasks, 'edit')
 
     new_task = input('Edit task: ')
     all_tasks[task_number-1] = new_task
-    addDescription(new_task, descriptions)
+    add_description(new_task, descriptions)
 
     print(f'Item {task_number} edited!')
 
-    displayTasks(all_tasks, descriptions)
-    newOperation(all_tasks, descriptions)
+    display_tasks(all_tasks, descriptions)
+    new_operation(all_tasks, descriptions)
 
-def removeTask(all_tasks, descriptions):
-    task_number = validTaskNumber(all_tasks, 'remove')
+def remove_task(all_tasks, descriptions):
+    task_number = valid_task_number(all_tasks, 'remove')
 
     del descriptions[all_tasks[task_number-1]]
     all_tasks.remove(all_tasks[task_number-1])
 
     print(f'\nItem {task_number} removed!')
 
-    displayTasks(all_tasks, descriptions)
-    newOperation(all_tasks, descriptions)
+    display_tasks(all_tasks, descriptions)
+    new_operation(all_tasks, descriptions)
 
-def addTask(all_tasks, descriptions):
+def add_task(all_tasks, descriptions):
     new_task = input('Add a task: ')
     all_tasks.append(new_task)
-    new_descriptions = addDescription(new_task, descriptions)
+    new_descriptions = add_description(new_task, descriptions)
 
-    displayTasks(all_tasks, new_descriptions)
-    newOperation(all_tasks, descriptions)
+    display_tasks(all_tasks, new_descriptions)
+    new_operation(all_tasks, descriptions)
         
 # Start application
-addTask(tasks, descriptions)
+add_task(tasks, descriptions)
